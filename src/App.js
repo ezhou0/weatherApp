7e2932956409d077c5a50246728032346448258d
Edwin <edwinzhou99@gmail.com>
2021-10-18 16:11:08 -0700

    get lat and long from geonavigation

--- src/App.js
+++ src/App.js
@@ -5,20 +5,50 @@ import './App.css';
 
 import Weather from './components/weather_component';
 
- 
+// const location_key = 90025; 
 const API_key = process.env.REACT_APP_API_KEY;
 
 class App extends React.Component{
   constructor(){
     super();
-    this.state = {};
-    this.getWeather();
+    this.state = {
+      lat: undefined,
+      long: undefined
+    };
+   
+    this.getLocation = this.getLocation.bind(this);
+    this.getCoordinates = this.getCoordinates.bind(this);
+     this.getWeather();
+  }
+
+
+  getLocation(){
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(this.getCoordinates);
+    }else{
+      alert("Geolocation is not supported on this browser");
+    }
+  }
+
+  getCoordinates(position){
+
+    this.setState({
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    })
+   
+
   }
 
   getWeather = async() => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=${API_key}`)
+    if(this.state.lat !== undefined &&this.state.long !== undefined){
+    
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/find?lat=${this.state.lat}&lon=${this.state.long}&cnt=1&&appid=${API_key}`)
     const response = await api_call.json();
+
     console.log(response);
+  }
+    
   }
   
   render(){
--- src/components/weather_component.jsx
+++ src/components/weather_component.jsx
@@ -6,6 +6,17 @@ const Weather = () =>{
     return(
         <div className = 'container'>
             <h1>Weather App</h1>
+            <div className = 'card'>
+                <h1>
+                    city, country
+                </h1>
+                <div>
+                    icon
+                </div>
+                <div>
+                    degrees
+                </div>
+            </div>
         </div>
     );
 };
