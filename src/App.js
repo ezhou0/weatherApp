68c9f1a775bf33d5608672f09e5cc9fe912ec771
Edwin <edwinzhou99@gmail.com>
2021-10-17 17:27:48 -0700

    add key

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+
 
 import Weather from './components/weather_component';
 
-function App() {
-  return (
-    <div className="App">
-      <Weather/>
-    </div>
-  );
+ 
+const API_key = process.env.REACT_REACT_API_KEY;
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {};
+  }
+
+  getWeather = async() => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=${API_key}`)
+    const response = await api_call.json();
+    console.log(response);
+  }
+  
+  render(){
+    return(
+      <div className="App">
+        <Weather/>
+      </div>
+    );
+  }
 }
 
 export default App;
